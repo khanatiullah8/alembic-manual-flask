537c90f8f3fdea989784b098da2e28b14d0ad19b
atiullah <khanatiullah8@gmail.com>
2025-05-06 13:58:02 +0530

    user model fix

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:12345@localhost/users"
 db = SQLAlchemy(app)
 
 class User(db.Model):
-  id = db.Column(db.Integer, primary_key=True),
-  username = db.Column(db.String(100), nullable=False),
+  id = db.Column(db.Integer, primary_key=True)
+  username = db.Column(db.String(100), nullable=False)
   email = db.Column(db.String(100), nullable=False, unique=True)
 
 if __name__ == "__main__":
